e53d952846fc93e2deb2df2e2b20788238b6f5d1
miyakogi <miyako.dev@gmail.com>
2017-08-30 21:55:58 +0900

    Update setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -134,7 +134,7 @@ html_theme = 'alabaster'
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'description': 'short description for this project',
+    'description': 'Headless chrome/chromium automation library',
     'github_user': 'miyakogi',
     'github_repo': 'pyppeteer',
     'github_banner': True,
