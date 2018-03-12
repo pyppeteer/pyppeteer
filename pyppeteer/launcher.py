86204633fca3abc3a5863056ed3cf5c7b8f612a6
CharlesB <charles.bridger@gmail.com>
2018-03-13 12:46:27 +1300

    Add retry attempts for removing temp user data
    Suppresses errors until end of attempts reached when closing session and removing temp user profile.

--- pyppeteer/launcher.py
+++ pyppeteer/launcher.py
@@ -104,8 +104,13 @@ class Launcher(object):
             self.chrome_args.extend(self.options['args'])
 
     def _cleanup_tmp_user_data_dir(self) -> None:
-        if self._tmp_user_data_dir and os.path.exists(self._tmp_user_data_dir):
-            shutil.rmtree(self._tmp_user_data_dir)
+        for retry in range(100):
+            if self._tmp_user_data_dir and os.path.exists(self._tmp_user_data_dir):
+                shutil.rmtree(self._tmp_user_data_dir)
+            else:
+                break
+        else:
+            raise IOError('Unable to remove Temporary User Data')
 
     async def launch(self) -> Browser:
         """Start chrome process and return `Browser` object."""
