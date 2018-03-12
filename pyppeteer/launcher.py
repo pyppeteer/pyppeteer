a387301c2617261e73a4c846398a1ee6afd2eef4
CharlesB <charles.bridger@gmail.com>
2018-03-13 12:50:27 +1300

    Add Ignore Errors

--- pyppeteer/launcher.py
+++ pyppeteer/launcher.py
@@ -106,7 +106,7 @@ class Launcher(object):
     def _cleanup_tmp_user_data_dir(self) -> None:
         for retry in range(100):
             if self._tmp_user_data_dir and os.path.exists(self._tmp_user_data_dir):
-                shutil.rmtree(self._tmp_user_data_dir)
+                shutil.rmtree(self._tmp_user_data_dir,ignore_errors=True)
             else:
                 break
         else:
