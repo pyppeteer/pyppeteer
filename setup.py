3d96c4fb9ec48d1f84108be52e9c0bb9cb11e25a
miyakogi <miyako.dev@gmail.com>
2018-03-14 07:28:46 +0900

    Bump version: 0.0.13 → 0.0.14

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ test_requirements = [
 
 setup(
     name='pyppeteer',
-    version='0.0.13',
+    version='0.0.14',
     description=('Headless chrome/chromium automation library '
                  '(unofficial port of puppeteer)'),
     long_description=readme,
