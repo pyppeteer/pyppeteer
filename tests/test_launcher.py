3d1ed91fec7b9f2400ba2ef2a83a2d46444da898
CharlesB <charles.bridger@gmail.com>
2018-03-19 23:01:31 +1300

    Update test_launcher.py
    fix flake8 error

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -48,7 +48,7 @@ class TestLauncher(unittest.TestCase):
         launcher = Launcher({'args': ['--user-data-dir=/path/to/profile']})
         self.check_default_args(launcher)
         self.assertIn('--user-data-dir=/path/to/profile', launcher.chrome_args)
-        self.assertIsNone(launcher._tmp_user_data_dir) 
+        self.assertIsNone(launcher._tmp_user_data_dir)
 
     @sync
     async def test_close_no_connection(self):
