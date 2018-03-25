4818d43b6e9560ac62cb4127e86a4c41df1a0927
miyakogi <miyako.dev@gmail.com>
2018-03-26 03:25:25 +0900

    Move tests for Dialog to test_dialog

--- tests/test_pyppeteer.py
+++ tests/test_pyppeteer.py
@@ -1282,35 +1282,6 @@ class TestPage(unittest.TestCase):
         self.page = await self.browser.newPage()
 
     @sync
-    async def test_alert(self):
-        def dialog_test(dialog):
-            self.assertEqual(dialog.type, 'alert')
-            self.assertEqual(dialog.defaultValue(), '')
-            self.assertEqual(dialog.message(), 'yo')
-            asyncio.ensure_future(dialog.accept())
-        self.page.on('dialog', dialog_test)
-        await self.page.evaluate('() => alert("yo")')
-
-    @sync
-    async def test_prompt(self):
-        def dialog_test(dialog):
-            self.assertEqual(dialog.type, 'prompt')
-            self.assertEqual(dialog.defaultValue(), 'yes.')
-            self.assertEqual(dialog.message(), 'question?')
-            asyncio.ensure_future(dialog.accept('answer!'))
-        self.page.on('dialog', dialog_test)
-        answer = await self.page.evaluate('() => prompt("question?", "yes.")')
-        self.assertEqual(answer, 'answer!')
-
-    @sync
-    async def test_prompt_dismiss(self):
-        def dismiss_test(dialog, *args):
-            asyncio.ensure_future(dialog.dismiss())
-        self.page.on('dialog', dismiss_test)
-        result = await self.page.evaluate('() => prompt("question?", "yes.")')
-        self.assertIsNone(result)
-
-    @sync
     async def test_user_agent(self):
         self.assertIn('Mozilla', await self.page.evaluate(
             '() => navigator.userAgent'))
