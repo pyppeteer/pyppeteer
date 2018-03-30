c2636b99cb56057e3dfdebe568e25acbb5a667ba
miyakogi <miyako.dev@gmail.com>
2018-03-30 12:37:40 +0900

    Ignore errors on browser termination

--- pyppeteer/launcher.py
+++ pyppeteer/launcher.py
@@ -189,8 +189,12 @@ class Launcher(object):
         """Terminate chromium process."""
         logger.debug('terminate chrome process...')
         if self.connection and self.connection._connected:
-            await self.connection.send('Browser.close')
-            await self.connection.dispose()
+            try:
+                await self.connection.send('Browser.close')
+                await self.connection.dispose()
+            except Exception:
+                # ignore errors on browser termination process
+                pass
         if self._tmp_user_data_dir and os.path.exists(self._tmp_user_data_dir):
             # Force kill chrome only when using temporary userDataDir
             self.waitForChromeToClose()
