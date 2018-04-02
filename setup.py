dd7d9e93b365cf784189f0bc0d69bc5def4a8bd0
miyakogi <miyako.dev@gmail.com>
2018-04-02 19:52:47 +0900

    Mark as alpha

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     zip_safe=False,
     keywords='pyppeteer',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
