cc78cc9ecc75b188dbdeaefc4a3d556f35a32525
miyakogi <miyako.dev@gmail.com>
2018-07-05 16:45:47 +0900

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.18',
+    VERSION: '0.0.19',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
