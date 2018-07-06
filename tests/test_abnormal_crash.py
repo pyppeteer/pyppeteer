2190cb6ec8042d0fb22157536000f975c5facad0
miyakogi <miyako.dev@gmail.com>
2018-07-06 21:19:32 +0900

    Fix test to pass Windows CI

--- tests/test_abnormal_crash.py
+++ tests/test_abnormal_crash.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import asyncio
 import logging
 import unittest
 
 from syncer import sync
 
 from pyppeteer import launch
+from pyppeteer.chromium_downloader import current_platform
 from pyppeteer.errors import NetworkError
 
 
@@ -19,6 +21,11 @@ class TestBrowserCrash(unittest.TestCase):
         await page.querySelector("title")
         browser.process.terminate()
         browser.process.wait()
+
+        if current_platform().startswith('win'):
+            # wait for terminating browser process
+            await asyncio.sleep(1)
+
         with self.assertRaises(NetworkError):
             await page.querySelector("title")
         with self.assertRaises(NetworkError):
