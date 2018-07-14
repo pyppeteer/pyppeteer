0bb81d93161f85a3e35dcc6b6b392173ba886567
miyakogi <miyako.dev@gmail.com>
2018-07-14 21:32:46 +0900

    Add Environment Variable section to the document

--- docs/server.py
+++ docs/server.py
@@ -12,7 +12,7 @@ watcher.pyinotify = None  # disable pyinotify
 docsdir = path.dirname(path.abspath(__file__))
 builddir = path.join(docsdir, '_build')
 build_cmd = [
-    'sphinx-build', '-q', '-j', '4', '-b', 'html',
+    'sphinx-build', '-q', '-j', 'auto', '-b', 'html',
     '-d', path.join(builddir, 'doctrees'),
     docsdir, path.join(builddir, 'html'),
 ]
