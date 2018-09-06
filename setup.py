1a0e26f427318839c4a44264a5da71329a945519
miyakogi <miyako.dev@gmail.com>
2018-09-07 08:56:26 +0900

    Explicitly support python 3.7 on setup.py

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     python_requires='>=3.5',
     test_suite='tests',
