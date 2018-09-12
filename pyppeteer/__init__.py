4dfabbc807ae24602d79b1d05a11f867a56bf454
miyakogi <miyako.dev@gmail.com>
2018-09-12 15:16:04 +0900

    Bump version: 0.0.23 → 0.0.24

--- pyppeteer/__init__.py
+++ pyppeteer/__init__.py
@@ -10,7 +10,7 @@ from appdirs import AppDirs
 
 __author__ = """Hiroyuki Takagi"""
 __email__ = 'miyako.dev@gmail.com'
-__version__ = '0.0.23'
+__version__ = '0.0.24'
 __chromimum_revision__ = '575458'
 __base_puppeteer_version__ = 'v1.6.0'
 __pyppeteer_home__ = os.environ.get(
