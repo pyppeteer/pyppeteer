2f4a6fd0c950be4ae63c4bb0c39e58976a32bbb6
Matt Marcus <mattmarcus58@gmail.com>
2020-03-04 17:02:58 -0700

    fully implement an async task queue

---
+++ pyppeteer/task_queue.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+from typing import Awaitable, Any
+
+logger = logging.getLogger(__name__)
+
+
+class TaskQueue:
+    def __init__(self, loop: asyncio.AbstractEventLoop = None):
+        self.loop = loop or asyncio.get_event_loop()
+        fut = self.loop.create_future()
+        fut.set_result(None)
+        self._task_chain = fut
+
+    def post_task(self, task: Awaitable[Any]):
+        async def run_awaitable(prev):
+            try:
+                await prev
+                await task
+                pass  # consider at least logging here
+            except Exception as e:
+                logger.error(f'Exception while evaluating task queue: {e}')
+
+        self._task_chain = self.loop.create_task(run_awaitable(self._task_chain))
+        return self._task_chain
