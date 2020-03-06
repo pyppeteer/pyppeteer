67262958a407f7deedcb0cf3a997d81258322e87
Matt Marcus <mattmarcus58@gmail.com>
2020-03-05 17:33:18 -0700

    best effort implementation of websockettransport class

---
+++ pyppeteer/websocket_transport.py
@@ -0,0 +1,34 @@
+import asyncio
+from typing import Iterable, Union, AsyncIterable
+
+from websockets import connect, WebSocketClientProtocol, Data
+
+
+class WebsocketTransport:
+    def __init__(self, ws: WebSocketClientProtocol):
+        self.onmessage = None
+        self.onclose = None
+        self.ws = ws
+
+    @classmethod
+    async def create(cls, uri: str, loop: asyncio.AbstractEventLoop = None) -> 'WebsocketTransport':
+        return cls(await connect(
+            uri=uri,
+            ping_interval=None,
+            max_size=256 * 1024 * 1024,  # 256Mb
+            loop=loop
+        ))
+
+    async def send(self, message: Union[Data, Iterable[Data], AsyncIterable[Data]]) -> None:
+        await self.ws.send(message)
+
+    async def close(self, code: int = 1000, reason: str = '') -> None:
+        if self.onclose:
+            await self.onclose()
+        await self.ws.close(code=code, reason=reason)
+
+    async def recv(self) -> Data:
+        data = await self.ws.recv()
+        if self.onmessage and data:
+            await self.onmessage(data)
+        return Data
