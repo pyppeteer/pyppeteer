8ae2404c29ca3f3367c4405b95c395acacd314e6
Matt Marcus <mattmarcus58@gmail.com>
2020-03-08 13:24:03 -0600

    replace all instances of chromium_downloader with browser_fetcher

--- tests/test_abnormal_crash.py
+++ tests/test_abnormal_crash.py
@@ -8,7 +8,7 @@ import unittest
 from syncer import sync
 
 from pyppeteer import launch
-from pyppeteer.chromium_downloader import current_platform
+from pyppeteer.browser_fetcher import current_platform
 from pyppeteer.errors import NetworkError
 import pytest
 
