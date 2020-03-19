d857df014a2dcaefbd23701d3f0e78db56748e4a
Matt Marcus <mattmarcus58@gmail.com>
2020-03-18 23:41:54 -0600

    use fixtures within fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,16 +6,22 @@ from syncer import sync
 from pyppeteer import launch, Browser
 from pyppeteer.page import Page
 
-DEFAULT_OPTIONS = {'args': ['--no-sandbox']}
+firefox = False
+launch_options = {'args': ['--no-sandbox']}
 
+if firefox:
+    launch_options['product'] = 'firefox'
+
+_browser: Browser = sync(launch(**launch_options))
 
 @pytest.fixture(scope='session')
-def browser() -> Browser:
-    return sync(launch(args='--no-sandbox'))
+def shared_browser() -> Browser:
+    yield _browser
+    sync(_browser.close())
 
 
 @pytest.fixture
-def isolated_page() -> Generator[Page, None, None]:
-    page = sync(browser.newPage())
+def isolated_page(shared_browser) -> Generator[Page, None, None]:
+    page = sync(shared_browser.newPage())
     yield page
     sync(page.close())
--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,44 +1,43 @@
-import pytest
 from syncer import sync
 
-from pyppeteer import connect, Browser
+from pyppeteer import connect
 
 
 @sync
-async def test_browser_version(browser: Browser):
-    version = await browser.version()
+async def test_browser_version(shared_browser):
+    version = await shared_browser.version()
     assert version.startswith('Headless')
 
 
 @sync
-async def test_browser_ua(browser: Browser):
-    ua = await browser.userAgent()
+async def test_browser_ua(shared_browser):
+    ua = await shared_browser.userAgent()
     assert 'WebKit' in ua or 'Gecko' in ua
 
 
 @sync
-async def test_browser_target(browser):
-    target = browser.target
+async def test_browser_target(shared_browser):
+    target = shared_browser.target
     assert target.type == 'browser'
 
 
 @sync
-async def test_browser_process(browser):
-    proc = browser.process
+async def test_browser_process(shared_browser):
+    proc = shared_browser.process
     assert proc.pid
 
 
 @sync
-async def test_browser_remote_process(browser):
-    browser_ws_endpoint = browser.wsEndpoint
+async def test_browser_remote_process(shared_browser):
+    browser_ws_endpoint = shared_browser.wsEndpoint
     remote_browser = await connect(browserWSEndpoint=browser_ws_endpoint)
     assert remote_browser.process is None
     await remote_browser.disconnect()
 
 
 @sync
-async def test_browser_connected(browser):
-    browser_ws_endpoint = browser.wsEndpoint
+async def test_browser_connected(shared_browser):
+    browser_ws_endpoint = shared_browser.wsEndpoint
     remote_browser = await connect(browserWSEndpoint=browser_ws_endpoint)
     assert remote_browser.isConnected
     await remote_browser.disconnect()
