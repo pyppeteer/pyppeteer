19d59fed957f78b7118e67b9eeb10d50593eacb6
Matt Marcus <mattmarcus58@gmail.com>
2020-03-21 22:37:03 -0600

    add explanation for new util func: gather_with_timeout

--- tests/utils.py
+++ tests/utils.py
@@ -20,6 +20,15 @@ def waitEvent(emitter, event_name):
 
 
 def gather_with_timeout(*aws: Awaitable, timeout: float = 10, **kwargs) -> Awaitable[List[Union[Task, Future]]]:
+    """
+    Similar to asyncio.gather, but with one key difference: It will timeout. A wrapped asyncio.gather approach was
+    chosen over asyncio.wait or asyncio.wait_for, etc. because asyncio.gather returns the awaitables in the same order
+    that they were passed in, which allows for closer analogous behaviour to JS's Promise.all
+    :param aws: Awaitables to gather
+    :param timeout: amount of time to wait, in seconds, before raising timeout error
+    :param kwargs: kwargs to pass to asyncio.gather
+    :return: same as asyncio.gather
+    """
     async def timeout_func(after: float) -> None:
         await asyncio.sleep(after)
         raise TimeoutError('timeout error occurred while gathering awaitables')
