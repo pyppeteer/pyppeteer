7588f811a606c11f1907220e7217f269d51bf997
Matt Marcus <mattmarcus58@gmail.com>
2020-03-26 22:32:16 -0600

    add test_dialog.py

--- tests/test_dialog.py
+++ tests/test_dialog.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import asyncio
 
 from syncer import sync
 
-from .base import BaseTestCase
 
-
-class TestDialog(BaseTestCase):
+class TestDialog:
     @sync
-    async def test_alert(self):
+    async def test_fires(self, isolated_page):
         def dialog_test(dialog):
             assert dialog.type == 'alert'
             assert dialog.defaultValue == ''
             assert dialog.message == 'yo'
             asyncio.ensure_future(dialog.accept())
 
-        self.page.on('dialog', dialog_test)
-        await self.page.evaluate('() => alert("yo")')
+        isolated_page.on('dialog', dialog_test)
+        await isolated_page.evaluate('() => alert("yo")')
 
     @sync
-    async def test_prompt(self):
+    async def test_accepting_prompt(self, isolated_page):
         def dialog_test(dialog):
             assert dialog.type == 'prompt'
             assert dialog.defaultValue == 'yes.'
             assert dialog.message == 'question?'
             asyncio.ensure_future(dialog.accept('answer!'))
 
-        self.page.on('dialog', dialog_test)
-        answer = await self.page.evaluate('() => prompt("question?", "yes.")')
+        isolated_page.on('dialog', dialog_test)
+        answer = await isolated_page.evaluate('() => prompt("question?", "yes.")')
         assert answer == 'answer!'
 
     @sync
-    async def test_prompt_dismiss(self):
-        def dismiss_test(dialog, *args):
-            asyncio.ensure_future(dialog.dismiss())
+    async def test_prompt_dismiss(self, isolated_page):
+        def dismiss_test(dialog, *_):
+            asyncio.create_task(dialog.dismiss())
 
-        self.page.on('dialog', dismiss_test)
-        result = await self.page.evaluate('() => prompt("question?", "yes.")')
+        isolated_page.on('dialog', dismiss_test)
+        result = await isolated_page.evaluate('() => prompt("question?")')
         assert result is None
