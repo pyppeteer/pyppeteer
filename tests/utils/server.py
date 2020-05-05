e6333fa6c0db61c12be831a5df91ccb420e4858b
Matt Marcus <mattmarcus58@gmail.com>
2020-05-04 22:40:56 -0600

    fix: asyncio.gather all subscribing functions to request path

--- tests/utils/server.py
+++ tests/utils/server.py
@@ -10,6 +10,7 @@ from urllib.parse import urlparse
 from aiohttp import web
 from aiohttp.log import web_logger
 from aiohttp.web_app import _Middleware
+from aiohttp.web_exceptions import HTTPNotFound
 from aiohttp.web_urldispatcher import UrlDispatcher
 
 
@@ -48,23 +49,49 @@ class WrappedApplication(web.Application):
                     args = [request]
                 func_res = precondition(*args)
             if isawaitable(func_res):
-                return await func_res
+                func_res = await func_res
+            return func_res
 
         return caller
 
     def add_pre_request_callback(self, path: str, precondition, should_return: bool):
         path = urlparse(path).path.strip('/')
+
         caller = self.get_pre_request_caller(precondition)
 
         if path not in self.pre_request_callbacks:
             self.pre_request_callbacks[path] = []
         self.pre_request_callbacks[path].append((caller, should_return))
 
-    def set_one_time_redirect(self, from_path: str, to_path: str):
-        def raiser():
-            raise web.HTTPFound(to_path)
+    def set_one_time_redirects(self, from_path: str, *path_items: str):
+        last_path = from_path
+        for to_path in path_items:
 
-        self.add_pre_request_callback(from_path, raiser, should_return=True)
+            def raiser():
+                raise web.HTTPFound(to_path)
+
+            self.add_pre_request_callback(last_path, raiser, should_return=True)
+            last_path = to_path
+
+    def set_one_time_response(self, path: str, response: str = None, status: int = 200):
+        def responder():
+            raisable_statuses = {
+                204: web.HTTPNoContent(),
+            }
+            if status in raisable_statuses:
+                raise raisable_statuses[status]
+            return web.Response(body=response, status=status)
+
+        self.add_pre_request_callback(path, responder, should_return=True)
+
+    def waitForRequest(self, path: str):
+        fut = asyncio.get_event_loop().create_future()
+
+        def resolve_fut(req):
+            fut.set_result(req)
+
+        self.add_pre_request_callback(path, resolve_fut, should_return=False)
+        return fut
 
 
 async def app_runner(assets_path, free_port: int = None):
@@ -73,15 +100,18 @@ async def app_runner(assets_path, free_port: int = None):
     async def static_file_serve(request):
         path = request.match_info['path']
         if path in request.app.pre_request_callbacks:
-            callbacks = sorted(
-                request.app.pre_request_callbacks.pop(path),
-                key=lambda item: (item[1], request.app.pre_request_callbacks[path].index(item)),
-            )
-            for callback, should_return in callbacks:
-                res = await callback(request)
+            callbacks = request.app.pre_request_callbacks.pop(path)
+            callbacks = sorted(callbacks, key=lambda item: (item[1], callbacks.index(item)),)
+            callback_fns, should_returns = zip(*callbacks)
+            gathered = await asyncio.gather(*[fn(request) for fn in callback_fns], return_exceptions=True)
+            for result, should_return in zip(gathered, should_returns):
+                if isinstance(result, Exception):
+                    raise result
                 if should_return:
-                    return res
+                    return result
 
+        if not (assets_path / request.match_info['path']).exists():
+            raise HTTPNotFound()
         return web.FileResponse(assets_path / request.match_info['path'])
 
     app = WrappedApplication()
