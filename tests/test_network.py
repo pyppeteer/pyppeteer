7839894a24725ddf931660cf26197fdc629f3ccc
mudro <vvgrygorchuk@gmail.com>
2020-06-06 12:10:53 +0300

    Add skeleton for network tests.

---
+++ tests/test_network.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Test Pyppeteer Network functionality.
+
+"""
+
+from syncer import sync
+
+from tests.conftest import chrome_only
+
+
+class TestPageEventsRequest:
+    pass
