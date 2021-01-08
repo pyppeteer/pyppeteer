f0282528091d6c789d08302b8adbcb55581c24d9
Mattwmaster58 <mattwmaster58@gmail.com>
2021-01-08 00:07:52 -0500

    fix some dev dependency

--- pyppeteer/__init__.py
+++ pyppeteer/__init__.py
@@ -15,7 +15,7 @@ except ModuleNotFoundError:
     from importlib_metadata import version
 
 try:
-    __version__ = version('wheel')
+    __version__ = version(__name__)
 except Exception:
     pass
 
