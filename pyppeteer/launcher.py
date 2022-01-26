0b717fd3bd56824005d15d90611151cc77dd6cb4
JavaScriptDude <tim.c.quinn@gmail.com>
2022-01-25 22:45:19 -0500

    Give Users Access To waitForChromeToClose() Call
    When users specify a `--user-data-dir=<path>` browser argument, `Launcher.waitForChromeToClose()` is not called and this causes  messages such as the following after the program has exited.
    
    ```
    Future exception was never retrieved
    future: <Future finished exception=NetworkError('Protocol error Target.sendMessageToTarget: Target closed.')>
    pyppeteer.errors.NetworkError: Protocol error Target.sendMessageToTarget: Target closed.
    ```
    
    This is also a problem on Windows as it causes the exit code from `sys.exit(<code>)` to be clobbered with code 120 under some conditions.
    
    This option will expose `waitForBrowserToClose` option to the Launcher.

--- pyppeteer/launcher.py
+++ pyppeteer/launcher.py
@@ -90,6 +90,7 @@ class Launcher(object):
         self.slowMo = options.get('slowMo', 0)
         self.timeout = options.get('timeout', 30000)
         self.autoClose = options.get('autoClose', True)
+        self.waitForBrowserToClose = options.get('waitForBrowserToClose', False)
 
         logLevel = options.get('logLevel')
         if logLevel:
@@ -213,9 +214,12 @@ class Launcher(object):
             except Exception as e:
                 # ignore errors on browser termination process
                 debugError(logger, e)
-        if self.temporaryUserDataDir and os.path.exists(self.temporaryUserDataDir):  # noqa: E501
-            # Force kill chrome only when using temporary userDataDir
+        _remUserDD = (self.temporaryUserDataDir and os.path.exists(self.temporaryUserDataDir))
+        if _remUserDD or self.waitForBrowserToClose:
+            print("Waiting for Chrome To Close ...")
             self.waitForChromeToClose()
+
+        if _remUserDD:  # noqa: E501
             self._cleanup_tmp_user_data_dir()
 
 
